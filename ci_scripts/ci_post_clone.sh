0a5a60f6e2a8588baa607889237c7dd8360009a4
Juergen <juergen@threedeers.com>
2024-06-21 13:04:19 +0200

    fixes

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,21 +2,21 @@
 
 set -x
 
-          API_KEY_ID="Q9Q9ZY5LCB"
-          API_ISSUER_ID="69a6de76-b502-47e3-e053-5b8c7c11a4d1"
-          KEY_CONTENT="MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQg2yfjm6Qlrt4O3zsA
-TEE9aBYPkgTXbeu5QRSptcIgwHagCgYIKoZIzj0DAQehRANCAARaPMyqTrYfxfRG
-Q3lGoQm2wiPEpvPWgi0vWZ17jcku4beMtiMGA/Qut4R0an7xwruZEtMYvZZcMoga
-coO2BCxl"
-          APP_ID="2Z97DX66EX.com.threedeers.myCloudApp.myNewCloudProject"
-          BUILD_NUMBER="1"
-          VERSION_NUMBER="1.0"
-          RELEASE_NOTES="these are my release notes...."
 
-    echo "start now"
+echo "start now"
+
+
+if [ $CI_WORKFLOW = 'deploy' ]
+then
+    echo "DEPLOY WORKFLOW"
+else
+    echo "NOT DEPLOY WORKFLOW"
+
+
+
 
 # Fetch JWT
-JWT=$(ruby -r 'base64' -r 'openssl' -r 'json' -e "
+export JWT=$(ruby -r 'base64' -r 'openssl' -r 'json' -e "
   header = { 'alg' => 'ES256', 'kid' => $API_KEY_ID }
   payload = { 'iss' => $API_ISSUER_ID, 'iat' => Time.now.to_i, 'exp' => Time.now.to_i + 20 * 60, 'aud' => 'appstoreconnect-v1' }
   key = OpenSSL::PKey::EC.new $KEY_CONTENT
@@ -33,7 +33,7 @@ curl -X PATCH \
   -d '{
     "data": {
       "type": "builds",
-      "id": "'$BUILD_ID'",
+      "id": "'$CI_BUILD_NUMBER'",
       "attributes": {
         "usesNonExemptEncryption": false,
         "individualTesters": [],
@@ -60,5 +60,10 @@ curl -X PATCH \
       }
     }
   }'
-  
+
   echo "done"
+
+
+
+
+
