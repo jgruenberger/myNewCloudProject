7cf1b4aa9398d291949998d506538bbce15fcbb6
Juergen <juergen@threedeers.com>
2024-06-21 08:21:34 +0200

    Initial Commit

---
+++ myNewCloudProject/myNewCloudProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myNewCloudProjectApp.swift
+//  myNewCloudProject
+//
+//  Created by JGRU on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct myNewCloudProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
