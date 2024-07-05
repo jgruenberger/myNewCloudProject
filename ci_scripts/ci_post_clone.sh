15038c5b9cb9d178fa22031bb91c5c273ca228d4
Juergen <juergen@threedeers.com>
2024-07-05 18:29:15 +0200

    dir switch

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -11,10 +11,13 @@ if [ $CI_WORKFLOW = "deploy" ]; then
   brew install fastlane
 
   # Verify the private key format
-  PRIVATE_KEY_PATH="fastlane/key.p8"
-  #printf "%s" "$KEY_CONTENT" > $PRIVATE_KEY_PATH
+  PRIVATE_KEY_PATH="./fastlane/key.p8"
+  printf "%s" "$KEY_CONTENT" > $PRIVATE_KEY_PATH
 
   echo "Private key path: $PRIVATE_KEY_PATH"
+  echo "Content of the private key file:"
+  cat $PRIVATE_KEY_PATH
+  echo "End content of the private key file:"
 
   openssl pkey -in "$PRIVATE_KEY_PATH" -noout -text || {
     echo "Failed to read private key. Please ensure the key is in PEM format and correctly specified."
@@ -28,18 +31,18 @@ if [ $CI_WORKFLOW = "deploy" ]; then
 
   # Navigate to the fastlane directory
   cd ../fastlane
-  fastlane ios promote_to_app_store
+  fastlane ios latest_build_number
+
+
 
   # Remove the temporary private key file
   rm $PRIVATE_KEY_PATH
   echo "Private key file removed: $PRIVATE_KEY_PATH"
   echo "Deployment done"
-  exit 0
+
 
 else
   echo "NOT DEPLOY WORKFLOW " $CI_WORKFLOW
-  exit 0
 fi
-
 
 exit 0
