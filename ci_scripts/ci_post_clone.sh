c42197cbe9eb3cf651fc1ba2bdfdb002dc7f1b02
Juergen <juergen@threedeers.com>
2024-07-07 22:07:11 +0200

    cleanup

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -11,43 +11,11 @@ if [ $CI_WORKFLOW = "deploy" ]; then
 
   brew install fastlane
 
-
-  # Navigate to the fastlane directory
-  cd ../fastlane
-
-
-
-  echo "****************** Contents of the fastlane directory:"
-  ls -l
-
-  # Verify the private key format
-#  PRIVATE_KEY_PATH="key.p8"
-#  printf "%s" "$KEY_CONTENT" > $PRIVATE_KEY_PATH
-#
-#  echo "Private key path: $PRIVATE_KEY_PATH"
-#  echo "Content of the private key file:"
-#  cat $PRIVATE_KEY_PATH
-#  echo "End content of the private key file:"
-#
-#  openssl pkey -in "$PRIVATE_KEY_PATH" -noout -text || {
-#    echo "Failed to read private key. Please ensure the key is in PEM format and correctly specified."
-#  }
-#
-#  echo "********************* Contents of the fastlane directory after key generation:"
-#  ls -l
-
   # Set environment variables
   export RELEASE_NOTES
   export VERSION_NUMBER
   export BUILD_NUMBER
-  #export PRIVATE_KEY_PATH
   export KEY_CONTENT
-
-  #export FASTLANE_PASSWORD
-  #export FASTLANE_USER
-  #export FASTLANE_SESSION
-
-  #echo "The fastlane session: $FASTLANE_SESSION"
 
   # Enable Fastlane debug mode
   #export FASTLANE_DEBUG=1
@@ -55,12 +23,7 @@ if [ $CI_WORKFLOW = "deploy" ]; then
 
   fastlane ios promote_to_app_store
 
-
-
-  # Remove the temporary private key file
-#  rm $PRIVATE_KEY_PATH
-#  echo "Private key file removed: $PRIVATE_KEY_PATH"
-#  echo "Deployment done"
+  echo "Deployment done"
 
 
 else
